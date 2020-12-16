6cc6456f45a383dcbeedee6387f83fcb4c9cc96d
xingwen4519 <xingwen4519@163.com>
2020-12-16 16:44:05 +0800

    s

---
+++ src/renderer/api.js
@@ -0,0 +1,54 @@
+import Axios from 'axios';
+
+Axios.defaults.baseURL = process.env.VUE_APP_URL;
+Axios.defaults.timeout = 10000;
+
+// 添加一个请求拦截器
+Axios.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = 'Bearer ' + token;
+  }
+  return config;
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
+
+// 添加一个响应拦截器
+Axios.interceptors.response.use(function (response) {
+  // Do something with response data
+  // //token过期，退出重新登录
+  // if (response.data.status.code === 401 || response.data.status.code === 402) {
+  //   localStorage.removeItem('token');
+  //   localStorage.removeItem('userInfo');
+  //   setTimeout(function () {
+  //     location.href = '/login';
+  //   }, 2000);
+  // }
+
+  //token错误，重新登录
+  if (response.data.status.code === 402) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userInfo');
+    setTimeout(function () {
+      location.href = '/login';
+    }, 2000);
+  }
+
+  //token过期--获取更新的token
+  const newtoken = response.headers.newtoken;
+  //token过期--如果token存在则存在localStorage
+  if (newtoken) {
+    console.log('token过期，已重新赋值');
+    localStorage.setItem('token', newtoken);
+  }
+  return response.data;
+}, function (error) {
+  // Do something with response error
+  return Promise.reject(error);
+});
+
+
+export default Axios;
--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -10,5 +10,44 @@ export default new Vuex.Store({
     createPersistedState(),
     createSharedMutations()
   ],
-  strict: process.env.NODE_ENV !== 'production'
+  strict: process.env.NODE_ENV !== 'production',
+  state: {
+    // 用户登录信息
+    token: localStorage.getItem('token') || '',
+    userInfo: JSON.parse(localStorage.getItem('userInfo')) || null,
+    //记住用户名密码
+    remUser: JSON.parse(localStorage.getItem('remUser')) || null,
+  },
+  mutations: {
+    //记住密码
+    setRemUser(state, remUser) {
+      state.remUser = remUser;
+    },
+    setUserInfo(state, userInfo) {
+      state.userInfo = userInfo;
+    },
+    setToken(state, token) {
+      state.token = token;
+    },
+  },
+  actions: {
+    setRemUser({ commit }, remUser) {
+      localStorage.setItem('remUser', JSON.stringify(remUser));
+      commit('setRemUser', remUser);
+    },
+    setUserInfo({ commit }, userInfo) {
+      localStorage.setItem('userInfo', JSON.stringify(userInfo));
+      commit('setUserInfo', userInfo);
+    },
+    setToken({ commit }, token) {
+      localStorage.setItem('token', token);
+      commit('setToken', token);
+    },
+    setSignOut({ commit }) {
+      localStorage.removeItem('token');
+      commit('setToken', '');
+      localStorage.removeItem('userInfo');
+      commit('setUserInfo', '');     
+    },
+  }
 })
